e92774d3f05685d8b3b465dfd760efcc5a4292af
KaioAmaralB <kaio411@hotmail.com>
2024-03-13 18:39:28 -0300

    Ajustando layer.tf e package.json

--- layer.tf
+++ layer.tf
@@ -1,10 +1,10 @@
 resource "null_resource" "install_layer_deps" {
     triggers = {
-        layer_build = filemd5("${path.module}/package.json")
+        layer_build = filemd5("${path.module}/layer/package.json")
     }
 
     provisioner "local-exec" {
-        working_dir = "${path.module}"
+        working_dir = "${path.module}/layer"
         command = "npm install --production"
     }
 }
@@ -12,26 +12,25 @@ resource "null_resource" "install_layer_deps" {
 data "archive_file" "lambdaLayer" {
     type = "zip"
     output_path = "files_lambda/lambda-layer.zip"
-    source_dir = "${path.module}"
+    source_dir = "${path.module}/layer"
     depends_on = [null_resource.install_layer_deps]
 }
 
-resource "aws_s3_object" "object" {
-  bucket = "techchallengestate-g27"
-  key    = "terraform-lambda/lambda"
-  source = data.archive_file.lambdaLayer.output_path
+# resource "aws_s3_object" "object" {
+#   bucket = "techchallengestate-g27"
+#   key    = "terraform-lambda/lambda"
+#   source = data.archive_file.lambdaLayer.output_path
 
-  etag = filemd5(data.archive_file.lambdaLayer.output_path)
+#   etag = filemd5(data.archive_file.lambdaLayer.output_path)
 
-  depends_on = [ data.archive_file.lambdaLayer ]
-}
+#   depends_on = [ data.archive_file.lambdaLayer ]
+# }
 
 resource "aws_lambda_layer_version" "lambdaLayer" {
   layer_name = "lambdaLayer"
-  s3_bucket = "techchallengestate-g27"
-  s3_key = "terraform-lambda/lambda"
+  filename = data.archive_file.lambdaLayer.output_path
   source_code_hash = data.archive_file.lambdaLayer.output_base64sha256
   compatible_runtimes = ["nodejs18.x"]
 
-  depends_on = [ aws_s3_object.object ]
+#   depends_on = [ aws_s3_object.object ]
 }

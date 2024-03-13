91792335e09416b292f76725cb0056e70bdbfcad
KaioAmaralB <kaio411@hotmail.com>
2024-03-13 19:19:28 -0300

    Ajustando package.json

--- lambda.tf
+++ lambda.tf
@@ -66,7 +66,7 @@ data "archive_file" "authLambdaArtefact" {
 
 resource "aws_lambda_function" "auth_lambda" {
   function_name = "authLambdaFunction"
-  handler = "index.handler"
+  handler = "handler"
   role    = aws_iam_role.lambda_execution_role.arn
   runtime = "nodejs18.x"
 

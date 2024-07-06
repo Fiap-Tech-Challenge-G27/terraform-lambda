d8a21f7b9e87d6bd2beb5dcdfa873d3d635ee50b
KaioAmaralB <kaio411@hotmail.com>
2024-07-05 21:14:13 -0300

    lambda anomização

---
+++ lambda/exclusao.js
@@ -0,0 +1,76 @@
+const { MongoClient } = require('mongodb');
+const { SecretsManagerClient, GetSecretValueCommand } = require("@aws-sdk/client-secrets-manager");
+
+const clientSecrets = new SecretsManagerClient({
+  region: "us-east-1"
+});
+
+const handler = async (event) => {
+
+  if (!event?.body) {
+    return {
+      statusCode: 422,
+      body: JSON.stringify({ error: "Missing body" }),
+    };
+  }
+
+  const { cpf } = JSON.parse(event.body);
+
+  if (!cpf) {
+    return {
+      statusCode: 422,
+      body: JSON.stringify({ error: "Missing cpf" }),
+    };
+  }
+
+  const result = await deleteCustomerByCpf(cpf);
+
+  if (!result.deletedCount) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({ error: "User not found or already deleted" }),
+    };
+  }
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify({ message: "User successfully deleted" }),
+  };
+};
+
+async function deleteCustomerByCpf(cpf) {
+  const secret_name = "documentdbcredentialsv2";
+
+  let response;
+  
+  try {
+    response = await clientSecrets.send(
+      new GetSecretValueCommand({
+        SecretId: secret_name,
+        VersionStage: "AWSCURRENT",
+      })
+    );
+  } catch (error) {
+    throw error;
+  }
+
+  const credentials = JSON.parse(response.SecretString);
+
+  const client = new MongoClient(credentials.urlCustomers, { retryWrites: false, useNewUrlParser: true, useUnifiedTopology: true });
+
+  try {
+    await client.connect();
+    const db = client.db(credentials.db);
+    const collection = db.collection('customers');
+    const result = await collection.deleteOne({ cpf: cpf });
+
+    return result;
+    
+  } catch (error) {
+    throw error;
+  } finally {
+    await client.close();
+  }
+}
+
+module.exports = { handler };
